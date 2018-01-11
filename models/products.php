0e7b0c0716915086243e0b5447b1af34ccb2abca
conejofoz <silviocoelho.udc@gmail.com>
2018-01-11 06:41:22 -0200

    aula 11 pag home ok

--- models/products.php
+++ models/products.php
@@ -2,14 +2,14 @@
 
 class Products extends model{
     
-    public function getList(){
+    public function getList($offset = 0, $limit = 3){
         $array = array();
         $sql = "SELECT *, "
                 . "( select brands.name from brands where brands.id = products.id_brand)"
                 . "as brand_name,"
                 . "( select categories.name from categories where categories.id = products.id_category)"
                 . "as category_name"
-                . " FROM products";
+                . " FROM products LIMIT $offset, $limit";
         $sql = $this->db->query($sql);
         if($sql->rowCount() > 0){
             $array = $sql->fetchAll();
@@ -58,6 +58,15 @@ class Products extends model{
         
         
         return $array;
+    }
+    
+    
+    
+    public function getTotal(){
+        $sql = "SELECT COUNT(*) as c FROM products";
+        $sql = $this->db->query($sql);
+        $sql = $sql->fetch();
+        return $sql['c'];
     }
 }
 
