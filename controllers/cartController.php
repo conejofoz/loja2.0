e6c36d330e941fe548dfb1802fb8a3e07f98dd0c
conejofoz <silviocoelho.udc@gmail.com>
2018-01-25 23:56:10 -0200

    aula 32 ok carrinho compras 1/3

--- controllers/cartController.php
+++ controllers/cartController.php
@@ -6,6 +6,7 @@
  * @copyright (c) year, Silvio Coelho 
  */
 class cartController extends Controller {
+
     private $user;
 
     public function __construct() {
@@ -15,12 +16,31 @@ class cartController extends Controller {
     public function index() {
         $store = new Store();
         $products = new Products();
-        
+
         $dados = $store->getTemplateData();
-        
-        
-        
+
+
+
         $this->loadTemplate('cart', $dados);
+    }
+
+    public function add() {
+        if (!empty($_POST['id_product'])) {
+            $id = intval($_POST['id_product']);
+            $qt = intval($_POST['qt_product']);
+
+            if (!isset($_SESSION['cart'])) {
+                $_SESSION['cart'] = array();
+            }
+
+            if (!isset($_SESSION['cart'][$id])) {
+                $_SESSION['cart'][$id] += $qt;
+            } else {
+                $_SESSION['cart'][$id] = $qt;
+            }
+        }
+        header("Location: " . BASE_URL . "cart");
+        exit;
     }
 
 }
--- views/cart.php
+++ views/cart.php
@@ -1,1 +1,4 @@
 <h1>carrinho</h1>
+<?php
+print_r($_SESSION['cart']);
+;?>
